b037dca14c33de0227eee33c70a7aac53514e90e
Gaven <gaven_yeung@163.com>
2022-02-11 17:26:23 +0800

    Initialize

---
+++ src/main/java/com/g/slidecaptcha/SlideCaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.g.slidecaptcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlideCaptchaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlideCaptchaApplication.class, args);
+    }
+
+}
